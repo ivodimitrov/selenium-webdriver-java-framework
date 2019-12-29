a49bda70eafb4ebe58717ca36481a01a309120d0
IvoDimitrov <ivopdimitrov@gmail.com>
2019-12-29 16:14:08 +0200

    Created LoginTests.java

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,6 +2,8 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 
@@ -10,6 +12,7 @@ public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
+    @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
@@ -25,12 +28,10 @@ public class BaseTests {
         //driver.manage().window().setSize(new Dimension(375, 812));
 
         homePage = new HomePage(driver);
-
-        driver.quit();
     }
 
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 }
