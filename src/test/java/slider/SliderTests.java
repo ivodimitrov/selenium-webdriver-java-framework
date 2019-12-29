db960627a893449a44352e0079bdc6587739ffc7
IvoDimitrov <ivopdimitrov@gmail.com>
2019-12-29 23:10:16 +0200

    Created SliderTests.java

--- src/test/java/slider/SliderTests.java
+++ src/test/java/slider/SliderTests.java
@@ -14,4 +14,12 @@ public class SliderTests extends BaseTests {
         sliderPage.setSliderValue(value);
         assertEquals(sliderPage.getSliderValue(), value, "Slider value is incorrect");
     }
+
+    @Test
+    public void testSlideToDecimalNumber() {
+        String value = "3.5";
+        var sliderPage = homePage.clickHorizontalSlider();
+        sliderPage.setSliderValue(value);
+        assertEquals(sliderPage.getSliderValue(), value, "Slider value is incorrect");
+    }
 }
