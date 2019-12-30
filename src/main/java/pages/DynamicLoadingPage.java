562018620cd0c77fc8d7968f50b9f5df97d0bca5
IvoDimitrov <ivopdimitrov@gmail.com>
2019-12-30 16:46:55 +0200

    Created DynamicLoadingPage.java & WaitTests.java

---
+++ src/main/java/pages/DynamicLoadingPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+public class DynamicLoadingPage {
+
+    private WebDriver driver;
+    private String linkXpath_Format = ".//a[contains(text(), '%s')]";
+    private By link_Example1 = By.xpath(String.format(linkXpath_Format, "Example 1"));
+    private By link_Example2 = By.xpath(String.format(linkXpath_Format, "Example 2"));
+
+    public DynamicLoadingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public DynamicLoadingExample1Page clickExample1() {
+        driver.findElement(link_Example1).click();
+        return new DynamicLoadingExample1Page(driver);
+    }
+
+    public DynamicLoadingExample2Page clickExample2() {
+        driver.findElement(link_Example2).click();
+        return new DynamicLoadingExample2Page(driver);
+    }
+
+    public DynamicLoadingExample2Page rightClickOnExample2Link() {
+        driver.findElement(link_Example2).sendKeys(Keys.CONTROL, Keys.RETURN);
+        return new DynamicLoadingExample2Page(driver);
+    }
+}
---
+++ src/test/java/wait/WaitTests.java
@@ -0,0 +1,23 @@
+package wait;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class WaitTests extends BaseTests {
+
+    @Test
+    public void testWaitUntilHidden() {
+        var loadingPage = homePage.clickDynamicLoading().clickExample1();
+        loadingPage.clickStart();
+        assertEquals(loadingPage.getLoadedText(), "Hello World!", "Loaded text incorrect");
+    }
+
+    @Test
+    public void testWaitUntilVisible() {
+        var loadingPage = homePage.clickDynamicLoading().clickExample2();
+        loadingPage.clickStart();
+        assertEquals(loadingPage.getLoadedText(), "Hello World!", "Loaded text incorrect");
+    }
+}
